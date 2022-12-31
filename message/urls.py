e347d18d9548874ae8bc73fb2769b588493ce96a
Mr. X <mohebmoallem1376@gmail.com>
2023-01-01 00:15:37 +0330

    add name for each urls

--- message/urls.py
+++ message/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('send/', views.SentMessagesListCreateView.as_view()),
-    path('received/', views.ReceivedMessagesListView.as_view()),
-    path('<int:pk>/', views.RetrieveUpdateDestroyMessageView.as_view()),
+    path('send/', views.SentMessagesListCreateView.as_view(), name='send_list_message'),
+    path('received/', views.ReceivedMessagesListView.as_view(), name='received_message'),
+    path('<int:pk>/', views.RetrieveUpdateDestroyMessageView.as_view(), name='retrieve_update_delete_message'),
 ]
