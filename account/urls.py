d15be3563e9589f75a7cb606cc2b92b1cdd8f87e
Mr. X <mohebmoallem1376@gmail.com>
2022-12-31 17:39:36 +0330

    add name

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
+from . import views
+
 urlpatterns = [
-    path('register/', views.RegisterApiView.as_view()),
+    path('register/', views.RegisterApiView.as_view(), name='user-register'),
     path('login/', TokenObtainPairView.as_view()),
     path('referesh/token/', TokenRefreshView.as_view()),
-    path('change-password/', views.ChangePasswordApiView.as_view()),
+    path('change-password/', views.ChangePasswordApiView.as_view(), name='user-change-password'),
 ]
